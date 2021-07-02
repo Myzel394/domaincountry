2f64f9506f480672db467618482bbdba59a39703
myzel394 <miguel.kra@gmail.com>
2021-07-02 14:02:38 +0200

    fix: CurrentTabPlugin removed

--- src/popup/main.js
+++ src/popup/main.js
@@ -3,7 +3,6 @@ import VueClassNames from "vue-classnames";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { faGlobe, faExchangeAlt, faWifi, faBroadcastTower, faClock } from "@fortawesome/free-solid-svg-icons"
-import CurrentTabPlugin from "@/plugins/CurrentTabPlugin";
 import store from "@/store";
 import App from "./App.vue";
 
@@ -17,5 +16,4 @@ createApp(App)
     .component("font-awesome-icon", FontAwesomeIcon)
     .use(VueClassNames)
     .use(store)
-    .use(CurrentTabPlugin)
     .mount("#app")

87fb0e3ecd9194c0186fe809f90e27b839fca772
myzel394 <miguel.kra@gmail.com>
2021-07-04 11:41:52 +0200

    added button animation

--- src/popup/components/InformationBoxes.vue
+++ src/popup/components/InformationBoxes.vue
@@ -111,5 +111,4 @@ export default {
         animation: none;
     }
 }
-
 </style>
--- src/popup/components/StateButtons.vue
+++ src/popup/components/StateButtons.vue
@@ -4,6 +4,7 @@
             <li
                 v-for="({name, active}) of buttons"
                 :key="name"
+                :class="$style.buttonAnimation"
             >
                 <ButtonState
                     :name="name"
@@ -42,6 +43,20 @@ export default {
 </script>
 
 <style module lang="scss">
+@keyframes buttonIn {
+    0% {
+        opacity: 0;
+        transform: scale(.9);
+    }
+    80% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 1;
+        transform: scale(1);
+    }
+}
+
 .wrapper {
     display: flex;
     align-items: center;
@@ -50,6 +65,27 @@ export default {
     > li {
         list-style: none;
         padding: 0 .6em;
+    }
+}
+
+.buttonAnimation {
+    opacity: 0;
+    transform: scale(.9);
+
+    animation: buttonIn .2s ease-out forwards;
+}
+
+.buttonAnimation:nth-of-type(2) {
+    animation-delay: .1s;
+}
+
+.buttonAnimation:nth-of-type(3) {
+    animation-delay: .2s;
+}
+
+@media (prefers-reduced-motion: reduce) {
+    .buttonAnimation {
+        animation: none;
     }
 }
 </style>

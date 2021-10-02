59394c8dd572b122911ef37a51197bf81a0beaf3
myzel394 <miguel.kra@gmail.com>
2021-10-02 20:05:53 +0200

    improved options

--- src/utils/options.ts
+++ src/utils/options.ts
@@ -27,7 +27,18 @@ const SCHEMA = yup.object().shape({
     fallbackQueryAPIUrl: yup.string()
         .url()
         .required(),
-})
+});
+
+const SCHEMA_WITH_DEFAULT = yup.object().shape({
+    allowBadge: yup.boolean()
+        .default(DEFAULT_VALUE.allowBadge),
+    badgeColor: yup.string()
+        .matches(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i)
+        .default(DEFAULT_VALUE.badgeColor),
+    fallbackQueryAPIUrl: yup.string()
+        .url()
+        .default(DEFAULT_VALUE.fallbackQueryAPIUrl),
+});
 
 const getRawData = async (key: string) => {
     const storage = getStorageMethod();
@@ -47,7 +58,7 @@ export const loadOptions = async (): Promise<Options> => {
     const rawData = await getRawData(KEY);
 
     try {
-        const cleanedData = await SCHEMA.validate(rawData);
+        const cleanedData = await SCHEMA_WITH_DEFAULT.validate(rawData);
         return Object.assign(DEFAULT_VALUE, cleanedData);
     } catch {
         return DEFAULT_VALUE;

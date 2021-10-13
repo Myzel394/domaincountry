d928c1e64e3ba79d220a25b085399ff159917af2
myzel394 <miguel.kra@gmail.com>
2021-10-13 15:39:18 +0200

    small improvement

--- src/background/backgroundEvents.ts
+++ src/background/backgroundEvents.ts
@@ -3,6 +3,7 @@ import { handleActiveTabs, handleStorageChange } from "@/utils/background";
 
 extension.runtime.onStartup.addListener(domainData.clearData);
 extension.storage.onChanged.addListener(handleStorageChange);
+extension.pageAction.onClicked.addListener(() => extension.browserAction.openPopup());
 
 // Handle on start
 handleActiveTabs();

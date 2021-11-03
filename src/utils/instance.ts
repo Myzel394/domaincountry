82c58d607b0838a0ac76f8e704130e4451dbf1f9
myzel394 <miguel.kra@gmail.com>
2021-11-03 21:24:22 +0100

    added timeout

--- src/utils/instance.ts
+++ src/utils/instance.ts
@@ -2,11 +2,14 @@ import axios, { AxiosInstance, AxiosAdapter } from "axios";
 import { throttleAdapterEnhancer } from "axios-extensions";
 import * as inMilliseconds from "in-milliseconds";
 
+const TIMEOUT = 5000;
+
 const createInstance = (): AxiosInstance => {
     const instance = axios.create({
         adapter: throttleAdapterEnhancer(axios.defaults.adapter as AxiosAdapter, {
             threshold: inMilliseconds.hours(5),
         }),
+        timeout: TIMEOUT,
     });
 
     return instance;

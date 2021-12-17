710b76aea85ea912ad0ec84fb619342a2814e20e
myzel394 <50424412+Myzel394@users.noreply.github.com>
2021-12-17 19:46:26 +0100

    improved initialization

--- src/utils/background/initializeTabs.ts
+++ src/utils/background/initializeTabs.ts
@@ -1,12 +1,47 @@
-import { handleTab } from "@/utils/background/index";
+import handleTab from "./handleTab";
+import getDomain from "../getDomain";
 
-const initializeTabs = async () => {
+const MAX_TABS_HANDLING_AT_ONCE = 40;
+
+const getAllOpenedTabsWithoutDuplicates = async () => {
+    const domains = new Set();
+
     const tabs = await extension.tabs.query({
         windowType: "normal",
-        // Only initialize active tabs, since if the user has many tabs open, they will get rate limited directly,
-        // and it also costs a lot of bandwidth
-        active: true,
     });
+
+    // Remove tabs with duplicate domains
+    return tabs.filter(tab => {
+        if (!tab.url) {
+            return false;
+        }
+
+        const domain = getDomain(tab.url);
+
+        if (!domains.has(domain)) {
+            domains.add(domain);
+            return true;
+        }
+
+        return false;
+    });
+}
+
+const initializeTabs = async () => {
+    const tabs = await (async () => {
+        const allTabs = await getAllOpenedTabsWithoutDuplicates();
+
+        if (allTabs.length <= MAX_TABS_HANDLING_AT_ONCE) {
+            return allTabs;
+        }
+
+        return await extension.tabs.query({
+            windowType: "normal",
+            // Only initialize active tabs, since if the user has many tabs open, they will get rate limited directly,
+            // and it also costs a lot of bandwidth
+            active: true,
+        });
+    })();
 
     await Promise.all(tabs.map(handleTab));
 }
